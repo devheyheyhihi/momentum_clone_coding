c30dd90056e0b12b10b92dff70d739b555a72b0e
heyhiheyhi <58721040+heyhiheyhi@users.noreply.github.com>
2020-10-19 18:12:15 +0900

    Update weather part
    Use API

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 const COORDS = "coords";
 
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
@@ -12,6 +25,7 @@ function handleGeoSucces(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(){
@@ -23,11 +37,12 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     }else {
-
+        const parseCoords = JSON.parse(loadedCoords)
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
